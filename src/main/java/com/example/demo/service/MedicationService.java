1abde9e0add796abae400138d91aee5512e80792
Murali <mscbalakrishnan@gmail.com>
2017-12-02 19:32:19 +0530

    medication find by name

--- src/main/java/com/example/demo/service/MedicationService.java
+++ src/main/java/com/example/demo/service/MedicationService.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.Medication;
 public interface MedicationService {
 	Medication save(Medication a);
 	Iterable<Medication> findAll();
+	Iterable<Medication> findByMedicationName(String name);
 	Medication findOne(long id);
 	void delete(long id);
 }
