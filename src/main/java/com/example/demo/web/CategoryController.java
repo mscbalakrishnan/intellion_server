f38667bb4e39c1411d1e63110c26e639b1296a84
muthumur <muthumur@gmail.com>
2017-12-07 13:35:21 +0530

    category crud changes by murali

--- src/main/java/com/example/demo/web/CategoryController.java
+++ src/main/java/com/example/demo/web/CategoryController.java
@@ -10,13 +10,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.domain.Category;
+import com.example.demo.domain.Doctor;
+import com.example.demo.domain.Medication;
 import com.example.demo.domain.Patient;
 import com.example.demo.domain.dto.CategoryDto;
+import com.example.demo.domain.dto.DoctorDto;
+import com.example.demo.domain.dto.MedicationDto;
 import com.example.demo.domain.dto.PatientDto;
+import com.example.demo.domain.dto.PrescriptionDto;
 import com.example.demo.service.AppointmentService;
+import com.example.demo.service.CategoryService;
 import com.example.demo.service.DoctorService;
 import com.example.demo.service.PatientService;
 
@@ -30,16 +39,32 @@ import com.example.demo.service.PatientService;
 public class CategoryController {
 	private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
 	@Autowired
-	private PatientService patientService;
-	@Autowired
-	private DoctorService doctorService;
-	@Autowired
-	private AppointmentService appointmentService;
+	private CategoryService categoryService;
 	
-	@GetMapping
+	
+	@GetMapping(value="/categorydto")
 	@ResponseBody
 	public List<CategoryDto> getAllCategories(HttpServletRequest request) {
 		List<CategoryDto> toReturn = new ArrayList<>();
+		List<Category> categories = (List<Category>) this.categoryService.findAll();
+		categories.forEach(c->toReturn.add(new CategoryDto(c)));
 		return toReturn;
 	}
+	
+	@PostMapping
+	@ResponseBody
+	public CategoryDto addCategory(@RequestBody CategoryDto categoryDto, HttpServletRequest request) {
+		logger.debug("*********** Received the Object to ADD {}" , categoryDto.toString());
+		Category category = new Category();
+		category.setName(categoryDto.getName());
+		category = this.categoryService.save(category);
+		return new CategoryDto(category);
+		
+	}
+	
+	
+	
+	
+	
+	
 }
