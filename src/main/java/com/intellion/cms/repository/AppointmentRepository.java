eaa1755b9cc5c5c073861358166bec54be00b4aa
nkumaraguru <kumaraguru@gmail.com>
2018-03-14 22:16:19 +0530

    appointment fix

--- src/main/java/com/intellion/cms/repository/AppointmentRepository.java
+++ src/main/java/com/intellion/cms/repository/AppointmentRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.intellion.cms.domain.Appointment;
@@ -20,7 +21,7 @@ public interface AppointmentRepository extends CrudRepository<Appointment, Long>
 	Iterable<Appointment> findByDoctor_Name_AndPatient_Name(String doctorName, String patietName);
 //	Page<Appointment> findAll(Pageable pageable);
 	@Query("select a from Appointment a where (a.time between :from and :to) and (a.doctor.name=:doctorName or a.patient.name=:patietName)")
-	Iterable<Appointment> findByTimeBetweenAndDoctorNameOrPatientName123(LocalDateTime from, LocalDateTime to, String doctorName, String patietName);
+	Iterable<Appointment> findByTimeBetweenAndDoctorNameOrPatientName123(@Param("from") LocalDateTime from, @Param("to") LocalDateTime to, @Param("doctorName") String doctorName, @Param("patietName") String patietName);
 	
 
 //	Page<Appointment> findByNameContainingAndCountryContainingAllIgnoringCase(String name,String country, Pageable pageable);
